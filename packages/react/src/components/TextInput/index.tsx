e14a10fe266a5e33af1f567db282890a45ad538d
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2024-10-02 13:28:47 -0300

    feat: Create TextInput Element

---
+++ packages/react/src/components/TextInput/index.tsx
@@ -0,0 +1,21 @@
+import { ComponentProps, ElementRef, forwardRef } from 'react'
+import { Input, Prefix, TextInputContainer } from './styles'
+
+export interface TextInputProps extends ComponentProps<typeof Input> {
+  prefix?: string
+  disabled?: boolean
+  placeholder?: string
+}
+
+export const TextInput = forwardRef<ElementRef<typeof Input>, TextInputProps>(
+  ({ prefix, ...props }, ref) => {
+    return (
+      <TextInputContainer>
+        {!!prefix && <Prefix>{prefix}</Prefix>}
+        <Input ref={ref} {...props} />
+      </TextInputContainer>
+    )
+  },
+)
+
+TextInput.displayName = 'TextInput'
