55c70c6c97db6d07cdbd0c76cb87f07fc6a330e5
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2024-10-02 11:41:55 -0300

    feat: Create Button Component

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Button, ButtonProps } from '@design-system/react'
+import { ArrowRight } from 'lucide-react'
+
+export default {
+  title: 'Form/Button',
+  component: Button,
+  args: {
+    children: 'Send',
+  },
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {}
+
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'secondary',
+    children: 'Create new',
+  },
+}
+
+export const Tertiary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'tertiary',
+    children: 'Cancel',
+  },
+}
+
+export const Small: StoryObj<ButtonProps> = {
+  args: {
+    size: 'sm',
+  },
+}
+
+export const WithIcon: StoryObj<ButtonProps> = {
+  args: {
+    children: (
+      <>
+        Próximo passo
+        <ArrowRight fontWeight="bold" />
+      </>
+    ),
+  },
+}
