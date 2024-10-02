e79c4bef021893d69ed00076cdaf5dfac0f504ef
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2024-10-02 20:38:19 -0300

    styles: Adjust some styles

---
+++ packages/react/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @edu-almeida-ui/react
+
+## 1.0.1
+
+### Patch Changes
+
+- Adjust some styles
--- packages/react/src/components/Box.tsx
+++ packages/react/src/components/Box.tsx
@@ -2,7 +2,7 @@ import { ComponentProps } from '../interfaces/ComponentProps'
 import { styled } from '../styles'
 
 export const Box = styled('div', {
-  padding: '$4',
+  padding: '$6',
   borderRadius: '$md',
   backgroundColor: '$gray800',
   border: '1px solid $gray600',
